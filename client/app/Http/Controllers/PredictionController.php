a46b2790ac2fc2c5d218252c00f7abc99df2dcf5
Martin Edgar Atole <meatole.cs@gmail.com>
2023-11-17 17:16:34 +0800

    feat: integrated plant disease prediction api from server

---
+++ client/app/Http/Controllers/PredictionController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class PredictionController extends Controller
+{
+    public function predict(Request $request)
+    {
+        $image = $request->file('image');
+
+        // Pwede i-encode yung image to base64
+        $imageBase64 = base64_encode(file_get_contents($image));
+
+        // POST request to FastAPI
+        $response = Http::post('http://localhost:3000/api/predict', [
+            'image_base64' => $imageBase64
+        ]);
+
+        if ($response->successful()) {
+            $result = $response->json();
+            return view('prediction-result', ['result' => $result]);
+        } else {
+            return back()->with('error', 'Sorry mayong laman AHAHAHA');
+        }
+    }
+}
---
+++ client/resources/views/prediction-result.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+@section('content')
+    <div>
+        <div>
+            <x-header />
+            <div class="position-relative px-6 py-24 h-[100vh]">
+                <div class="text-center pb-5 border-b">
+                    <img src="{{ asset('assets/dummy_prediction.png') }}" alt="This contains the image predicted"
+                        class="max-w-[18rem] mx-auto pb-3">
+                    <small class="text-[#8A8A8A]">Disease Prediction:</small>
+                    <h1 class=" font-bold text-2xl pt-1"> {{ $result['prediction'] ?? 'No prediction available' }}</h1>
+                </div>
+
+                <div class="text-center">
+                    <div class="py-4">
+                        <button>
+                            <svg class="mx-auto" xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+                                viewBox="0 0 24 24" fill="none">
+                                <path d="M12.5 5V19M20 12H5" stroke="#2CA464" stroke-linecap="round"
+                                    stroke-linejoin="round" />
+                            </svg>
+                            <small>Add to History</small>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <x-bottom-nav />
+    </div>
+@endsection
