dd939a3adfc9583980b5ae29043777e4717d7082
Martin Edgar Atole <meatole.cs@gmail.com>
2023-11-17 18:27:23 +0800

    Fix: minor ui in dashboard and header

--- client/resources/views/components/header.blade.php
+++ client/resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <div class="fixed sm:w-[500px] px-6 flex bg-white items-center justify-between h-[65px] w-full border-b mb-6">
     <div class="flex gap-x-3 font-bold">
-        @if (request()->is('history') || request()->is('prediction/*') || request()->is('chatbot'))
+        @if (request()->is('history') || request()->is('prediction-result') || request()->is('chatbot'))
             <a href="{{ url()->previous() }}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                     <path d="M20 12H4M4 12L10 6M4 12L10 18" stroke="#3D3D3D" stroke-width="1.5" stroke-linecap="round"
@@ -13,7 +13,7 @@
             <img src="{{ asset('assets/greengard_logo.png') }}" width="100" alt="">
         @elseif (request()->is('history'))
             <p>History</p>
-        @elseif (request()->is('prediction/*'))
+        @elseif (request()->is('prediction-result'))
             <p>Prediction</p>
         @elseif (request()->is('chatbot'))
             <p>Chatbot</p>
--- client/resources/views/dashboard.blade.php
+++ client/resources/views/dashboard.blade.php
@@ -4,46 +4,78 @@
         <x-header />
         <div class="position-relative px-6 py-24">
             <div>
-                <form action="{{ route('prediction-result') }}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <input type="file" name="image" required>
-                    <button type="submit">Predict</button>
-                </form>
+                <div class="pb-[1rem]">
+                    <p class="font-bold pb-0">Make Prediction</p>
+                    <small class="text-[#8A8A8A]">Insert a photo to predict its disease</small>
+                    <form action="{{ route('prediction-result') }}" method="POST" enctype="multipart/form-data" class="flex gap-3">
+                        @csrf
+                        <input
+                            class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                            name="image" type="file" required>
+                        <button type="submit" class=" bg-green-500 p-2 px-5 text-white rounded-md">Predict</button>
+
+                    </form>
+                </div>
 
                 <x-history-card />
 
                 {{-- Camera floating button --}}
                 <div class="fixed sm:w-[450px] text-right bottom-[16%]">
-                    <button class="p-6 rounded-full bg-green-600 shadow-2xl" id="cameraButton">
-                        <input type="file" id="fileInput" capture="user" accept="image/*" style="display: none;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="31" height="31" viewBox="0 0 31 31"
-                            fill="none">
-                            <path
-                                d="M12.9167 28.4167C8.04552 28.4167 5.60994 28.4167 4.09665 26.9034C2.58337 25.3902 2.58337 24.2461 2.58337 19.375"
-                                stroke="white" stroke-width="1.5" stroke-linecap="round" />
-                            <path
-                                d="M28.4167 19.375C28.4167 24.2461 28.4167 25.3902 26.9034 26.9034C25.3902 28.4167 22.9545 28.4167 18.0834 28.4167"
-                                stroke="white" stroke-width="1.5" stroke-linecap="round" />
-                            <path
-                                d="M18.0834 2.58331C22.9545 2.58331 25.3902 2.58331 26.9034 4.09659C28.4167 5.60988 28.4167 6.75379 28.4167 11.625"
-                                stroke="white" stroke-width="1.5" stroke-linecap="round" />
-                            <path
-                                d="M12.9167 2.58331C8.04552 2.58331 5.60994 2.58331 4.09665 4.09659C2.58337 5.60988 2.58337 6.75379 2.58337 11.625"
-                                stroke="white" stroke-width="1.5" stroke-linecap="round" />
-                            <path d="M2.58337 15.5H28.4167" stroke="white" stroke-width="1.5" stroke-linecap="round" />
-                        </svg>
-                    </button>
-                </div>
+                    {{-- <form action="{{ route('prediction-result') }}" method="POST" enctype="multipart/form-data">
+                        <button class="p-6 rounded-full bg-green-600 shadow-2xl" id="cameraButton">
+                            <input type="file" id="file" capture="user" accept="image/*" style="display: none;">
 
+                        </button>
+                    </form> --}}
+                    <form id="cameraForm" action="{{ route('prediction-result') }}" method="POST"
+                        enctype="multipart/form-data">
+                        @csrf
+                        <button class="p-6 rounded-full bg-green-600 shadow-2xl" id="cameraButton" type="button">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="31" height="31" viewBox="0 0 31 31"
+                                fill="none">
+                                <path
+                                    d="M12.9167 28.4167C8.04552 28.4167 5.60994 28.4167 4.09665 26.9034C2.58337 25.3902 2.58337 24.2461 2.58337 19.375"
+                                    stroke="white" stroke-width="1.5" stroke-linecap="round" />
+                                <path
+                                    d="M28.4167 19.375C28.4167 24.2461 28.4167 25.3902 26.9034 26.9034C25.3902 28.4167 22.9545 28.4167 18.0834 28.4167"
+                                    stroke="white" stroke-width="1.5" stroke-linecap="round" />
+                                <path
+                                    d="M18.0834 2.58331C22.9545 2.58331 25.3902 2.58331 26.9034 4.09659C28.4167 5.60988 28.4167 6.75379 28.4167 11.625"
+                                    stroke="white" stroke-width="1.5" stroke-linecap="round" />
+                                <path
+                                    d="M12.9167 2.58331C8.04552 2.58331 5.60994 2.58331 4.09665 4.09659C2.58337 5.60988 2.58337 6.75379 2.58337 11.625"
+                                    stroke="white" stroke-width="1.5" stroke-linecap="round" />
+                                <path d="M2.58337 15.5H28.4167" stroke="white" stroke-width="1.5" stroke-linecap="round" />
+                            </svg>
+                        </button>
+                        <input type="file" id="file" name="image" capture="user" accept="image/*"
+                            style="display: none;">
+                    </form>
+                </div>
             </div>
         </div>
     </div>
     <x-bottom-nav />
     </div>
 
+    <style scoped>
+        input#file-upload-button {
+            background: #2ca464 !important;
+        }
+    </style>
     <script>
+        // document.getElementById('cameraButton').addEventListener('click', function() {
+        //     document.getElementById('fileInput').click();
+        // });
         document.getElementById('cameraButton').addEventListener('click', function() {
-            document.getElementById('fileInput').click();
+            document.getElementById('file').click();
+        });
+
+        document.getElementById('file').addEventListener('change', function() {
+            if (this.files && this.files[0]) {
+                // Submit the form automatically when the file is chosen
+                document.getElementById('cameraForm').submit();
+            }
         });
     </script>
 @endsection
