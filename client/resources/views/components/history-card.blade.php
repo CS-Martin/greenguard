3b37b4cc6cb2ca272667e83027ee6fcafc452154
meatole <meatole@gbox.adnu.edu.ph>
2023-11-19 14:58:17 +0800

    fix: prediction view layout

--- client/resources/views/components/history-card.blade.php
+++ client/resources/views/components/history-card.blade.php
@@ -1,80 +1,82 @@
+<div>
 
-<table class="table-auto">
-    <tbody>
-        <tr>
-            <div class="relative">
-                <button data-modal-target="popup-modal" data-modal-toggle="popup-modal"
-                    class="absolute top-0 right-0 text-black font-medium rounded-lg text-sm hover:bg-gray-200 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    type="button">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+    <table class="table-auto">
+        <tbody>
+            <tr>
+                <div class="relative">
+                    <button data-modal-target="popup-modal" data-modal-toggle="popup-modal"
+                        class="absolute top-0 right-0 text-black font-medium rounded-lg text-sm hover:bg-gray-200 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        type="button">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+                        </svg>
+
+                    </button>
+                    <a href="{{ route('prediction', ['id' => $prediction->id]) }}">
+                        <div class="py-3  border-b hover:bg-gray-100">
+                            <div class="flex justify-between mb-3">
+                                <p class="font-semibold"> {{ $prediction->result }} </p>
+                            </div>
+                            <div class="flex justify-between">
+                                <small class="text-[#8A8A8A]"> {{ $prediction->created_at->format('M d, Y') }} </small>
+                                <small class="text-right text-[#8A8A8A]"> {{ $prediction->created_at->format('g:i A') }}
+                                </small>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </tr>
+        </tbody>
+    </table>
+
+    {{-- Delete modal --}}
+    <div id="popup-modal" tabindex="-1"
+        class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative p-4 w-full max-w-md max-h-full">
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <button type="button"
+                    class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-hide="popup-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                     </svg>
-
+                    <span class="sr-only">Close modal</span>
                 </button>
-                <a href="{{ route('prediction', ['id' => $prediction->id]) }}">
-                    <div class="py-3  border-b hover:bg-gray-100">
-                        <div class="flex justify-between mb-3">
-                            <p class="font-semibold"> {{ $prediction->result }} </p>
+                <div class="p-4 md:p-5 text-center">
+                    <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="red" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure do you want to delete
+                        this prediction?</h3>
+
+
+                    <div class="flex justify-center items-center">
+                        <div class="flex">
+                            <form action="{{ route('prediction.delete', ['id' => $prediction->id]) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button data-modal-hide="popup-modal" type="submit"
+                                    class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2">
+                                    Yes, I'm sure
+                                </button>
+                            </form>
+                            <button data-modal-hide="popup-modal" type="button"
+                                class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No,
+                                cancel</button>
                         </div>
-                        <div class="flex justify-between">
-                            <small class="text-[#8A8A8A]"> {{ $prediction->created_at->format('M d, Y') }} </small>
-                            <small class="text-right text-[#8A8A8A]"> {{ $prediction->created_at->format('g:i A') }}
-                            </small>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </tr>
-    </tbody>
-</table>
-
-{{-- Delete modal --}}
-<div id="popup-modal" tabindex="-1"
-    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-md max-h-full">
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button type="button"
-                class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                data-modal-hide="popup-modal">
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                </svg>
-                <span class="sr-only">Close modal</span>
-            </button>
-            <div class="p-4 md:p-5 text-center">
-                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="red" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                </svg>
-                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure do you want to delete
-                    this prediction?</h3>
-
-
-                <div class="flex justify-center items-center">
-                    <div class="flex">
-                        <form action="{{ route('prediction.delete', ['id' => $prediction->id]) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button data-modal-hide="popup-modal" type="submit"
-                                class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2">
-                                Yes, I'm sure
-                            </button>
-                        </form>
-                        <button data-modal-hide="popup-modal" type="button"
-                            class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No,
-                            cancel</button>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    @if ($prediction->count() <= 0)
-        <div class="bg-gray-100 w-full rounded-lg p-4  text-center">
-            <p class=" text-[#8A8A8A]">No Detection History</p>
-        </div>
-    @endif
+        @if ($prediction->count() <= 0)
+            <div class="bg-gray-100 w-full rounded-lg p-4  text-center">
+                <p class=" text-[#8A8A8A]">No Detection History</p>
+            </div>
+        @endif
+</div>
