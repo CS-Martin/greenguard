42ad0a27bcf30eb425da77dee92c81cc2e6ee26c
Martin Edgar Atole <meatole.cs@gmail.com>
2023-12-15 13:20:05 +0800

    update: host:<port> will automatically redirect to /login page

--- client/app/Providers/RouteServiceProvider.php
+++ client/app/Providers/RouteServiceProvider.php
@@ -35,6 +35,10 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            if ($this->app->environment('local')) {
+            Route::redirect('/', '/login');
+            }
         });
     }
 }
